c2d530533dfc069ba633168cca8d534c1cdedd53
arnaudlange <langelier.arnaud@gmail.com>
2022-02-12 22:04:49 +0100

    First version of my resume website

---
+++ components/titleIcons.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="title-icons">
+    <i><b-link href="https://github.com/arnaudlange" target="_blank" class="bi bi-github"></b-link></i>
+    <i><b-link href="https://www.linkedin.com/in/alangelier/" target="_blank" class="bi bi-linkedin"></b-link></i>
+    <i><b-link href="mailto:langelier.arnaud@gmail.com" class="bi bi-envelope"></b-link></i>
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'title-icons',
+  }
+
+</script>
+
+<style scoped>
+@import url('bootstrap-icons');
+</style>
