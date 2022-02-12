01751528995c2ce170ac51027d2a0284ef4b1478
arnaudlange <langelier.arnaud@gmail.com>
2022-02-13 00:59:35 +0100

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: 'https://arnaudlange.github.io/resume-website/resume-website/'
+    base: '/resume-website/'
   },
   static: {
     prefix: false
